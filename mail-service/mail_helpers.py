d60a71b5efdd363610aeb90ff6ce9a5612151e6b
Jonathan Langens <flowofcontrol@gmail.com>
2017-02-25 22:04:07 +0100

    added fetch and send mail functionality

--- mail-service/mail_helpers.py
+++ mail-service/mail_helpers.py
@@ -5,7 +5,18 @@ import email
 import datetime
 import uuid
 import helpers
+import json
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
 
+
+# helper functino that saves a mail to the triple store
+# it expects the following parameters:
+# sender, date, subject and content and creates
+# a new UUID for each mail that it saves
 def save_mail(sender, date, subject, content):
     str_uuid = str(uuid.uuid4())
     insert_query = "INSERT DATA\n{\nGRAPH <http://mu.semte.ch/application>\n{\n<http://mail.com/examples/mail/" + str_uuid + "> a <http://mail.com/Mail>;\n"
@@ -13,12 +24,16 @@ def save_mail(sender, date, subject, content):
     insert_query += "<http://mail.com/date> \"" + date + "\";\n"
     insert_query += "<http://mail.com/content> \"" + content + "\";\n"
     insert_query += "<http://mail.com/subject> \"" + subject + "\";\n"
+    insert_query += "<http://mail.com/ready> \"yes\";\n"
     insert_query += "<http://mu.semte.ch/vocabularies/core/uuid> \"" + str_uuid + "\".\n"
     insert_query += "}\n}"
-    print "query:\n", insert_query
     helpers.update(insert_query)
     
 
+# helper functino that expects a python mail library
+# mailbox object
+# this function will call the save_mail function for
+# every mail in the mailbox
 def process_mailbox(mailbox):
   rv, data = mailbox.search(None, "ALL")
   if rv != 'OK':
@@ -36,3 +51,67 @@ def process_mailbox(mailbox):
       content = content.replace('\n','')
       
       save_mail(msg['From'], msg['Date'], msg['Subject'], content)
+
+# the load mail function loads a mail from the database
+# if it cannot find a mail in the database for the given
+# uuid it will return nil
+def load_mail(uri):
+    # this query will find the mail (if it exists)
+    select_query = "SELECT DISTINCT ?uuid ?from ?to ?ready ?subject ?content\n"
+    select_query += "WHERE \n{\n"
+    select_query += "<" + str(uri) + "> <http://mail.com/from> ?from;\n"
+    select_query += "a <http://mail.com/Mail>;\n"
+    select_query += "<http://mail.com/content> ?content;\n"
+    select_query += "<http://mail.com/subject> ?subject;\n"
+    select_query += "<http://mail.com/to> ?to;\n"
+    select_query += "<http://mail.com/ready> ?ready;\n"
+    select_query += "<http://mu.semte.ch/vocabularies/core/uuid> ?uuid.\n"
+    select_query += "}"
+
+    # execute the query...
+    result = helpers.query(select_query)
+
+    # if the length of the result array is 0 we return nil
+    if len(result['results']['bindings']) < 1:
+        return {}
+
+    # I should probably check here but for a quick test application
+    # it doesn't matter that much. If there is more than 1 result
+    # that would indicate a data error
+    bindings = result['results']['bindings'][0]
+
+    # we extract an object
+    mail = dict()
+    mail['uuid'] = bindings['uuid']['value']
+    mail['from'] = bindings['from']['value']
+    mail['ready'] = bindings['ready']['value']
+    mail['to'] = bindings['to']['value']
+    mail['subject'] = bindings['subject']['value']
+    mail['content'] = bindings['content']['value']
+
+    return mail
+
+# I have copied the followig function entirely from
+# http://naelshiab.com/tutorial-send-email-python/
+# the function has been slightly modified to take the
+# dictionary mail object from load_mail into account
+def send_mail(mail, email_address, email_password):
+    
+    fromaddr = email_address
+    toaddr = mail['to']
+    
+    msg = MIMEMultipart()
+    
+    msg['From'] = mail['from']
+    msg['To'] = mail['to']
+    msg['Subject'] = mail['subject']
+    
+    body = mail['content']
+    msg.attach(MIMEText(body, 'plain'))
+    
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, email_password)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
--- mail-service/web.py
+++ mail-service/web.py
@@ -6,11 +6,18 @@ import datetime
 import uuid
 import helpers
 import mail_helpers
+import json
+from flask import request
 
+
+EMAIL_ADDRESS = ""
+EMAIL_PWD = ""
+
+# /fetchMails route
+# will load all mails found in the INBOX of the email address
+# defined above in to the triple store
 @app.route("/fetchMails")
 def fetchMailMethod():
-    EMAIL_ADDRESS = "maildude.tenforce@gmail.com"
-    EMAIL_PWD = "TenforceS3cr3tZ"
 
     MAIL_SERVER = imaplib.IMAP4_SSL('imap.gmail.com')
 
@@ -27,3 +34,43 @@ def fetchMailMethod():
     MAIL_SERVER.logout()
 
     return "ok"
+
+# /process_delta [POST]
+# will monitor delta reports from the triple store
+# if a reports indicates that a mail is ready to be
+# sent this service will fetch all necessary information from
+# the store and send the mail
+@app.route("/process_delta", methods=['POST'])
+def processDelta():
+    # The delta will be send in the body of this request, we parse it and put the
+    # result in a variable.
+    delta_report = json.loads(request.data)
+
+    # From the delta's we will be able to make out which URI's should describe
+    # a mail that we will have to send. We keep all these URI's in a list.
+    mails_to_send = set()
+
+    # The predicate that describes a mail as ready for sending is:
+    predicate_mail_is_ready = "http://mail.com/ready"
+
+    # The value for that predicate that we aim for:
+    value_mail_is_ready = "yes"
+
+    # The delta report can contain more than 1 delta. We will loop over each
+    # delta and then loop over the inserted triples for that delta.
+    #
+    # If the predicate matches our predicate and the object matches our object
+    # we will add the subject to the mails_to_send list.
+    for delta in delta_report['delta']:
+        for triple in delta['inserts']:
+            if(triple['p']['value'] == predicate_mail_is_ready):
+                if(triple['o']['value'] == value_mail_is_ready):
+                    mails_to_send.add (triple['s']['value'])
+
+    # fetching and sending the mails for every URI found
+    for uri in mails_to_send:
+        mail = mail_helpers.load_mail(uri)
+        if 'uuid' in mail.keys():
+            mail_helpers.send_mail(mail, EMAIL_ADDRESS, EMAIL_PWD)
+        
+    return "delta processed"
